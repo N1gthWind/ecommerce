eea84105913061b67dd271291b78fb647b7e2f31
Nightwind <terezsak@gmail.com>
2022-11-24 02:45:04 +0100

    Dashboard statistics added!

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\User;
 use Inertia\Inertia;
 
 class AdminDashboardController extends Controller
@@ -11,13 +13,20 @@ class AdminDashboardController extends Controller
 
     public function index()
     {
+
+        $users_count = User::count();
+        $products_count = Product::count();
+        $orders_count = Order::count();
+        $not_completed_orders = Order::whereNot('status', 'COMPLETED')->count();
+
         return Inertia::render('Admin/Dashboard', [
             'logo' => asset('assets/logo.png'),
             'avatar' => asset('assets/images/avatar.jpg'),
+            'users_count' => $users_count,
+            'products_count' => $products_count,
+            'orders_count' => $orders_count,
+            'not_completed_orders' => $not_completed_orders,
         ]);
     }
-
-
-    
 
 }
--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -15,14 +15,127 @@ const props = defineProps({
         type: String,
         default: null,
         required: true,
-    }
+    },
+    orders_count: {
+        type: Number,
+        required: true,
+    },
+    users_count: {
+        type: Number,
+        required: true,
+    },
+    products_count: {
+        type: Number,
+        required: true,
+    },
+    not_completed_orders: {
+        type: Number,
+        required: true,
+    },
 });
 
 </script>
 
 <template>
     <AdminLayout :logo="$page.props.assets.logo" :avatar="$page.props.assets.avatar">
+        <div class="page-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-6">
+                        <!-- Card group -->
+                        <div class="card-group">
 
+                            <!-- Card -->
+                            <div class="card mb-4">
+
+                                <!-- Card content -->
+                                <div class="card-body">
+
+                                    <!-- Title -->
+                                    <h6 class="card-title">Users:</h6>
+                                    <!-- Text -->
+
+
+                                    <p class="card-text blue-text"><i class="fas fa-user fa-2x"></i><span
+                                            class="ml-2" style="font-size: 30px;">{{ users_count }}</span></p>
+
+                                </div>
+                                <!-- Card content -->
+
+                            </div>
+                            <!-- Card -->
+
+                            <!-- Card -->
+                            <div class="card mb-4">
+
+                                <!-- Card content -->
+                                <div class="card-body">
+                                    <!-- Title -->
+                                    <h6 class="card-title">Products: </h6>
+                                    <!-- Text -->
+                                    <p class="card-text red-text">
+                                        <i class="fas fa-box fa-2x"></i>
+                                        <span class="ml-2" style="font-size: 30px;">{{ products_count }}</span>
+                                    </p>
+                                </div>
+                                <!-- Card content -->
+
+                            </div>
+                            <!-- Card -->
+
+                        </div>
+                        <!-- Card group -->
+                    </div>
+
+
+
+                    <div class="col-6">
+                        <!-- Card group -->
+                        <div class="card-group">
+
+                            <!-- Card -->
+                            <div class="card mb-4">
+
+                                <!-- Card content -->
+                                <div class="card-body">
+
+                                    <!-- Title -->
+                                    <h6 class="card-title">Orders:</h6>
+                                    <!-- Text -->
+
+
+                                    <p class="card-text green-text"><i class="fas fa-bag-shopping fa-2x"></i><span
+                                            class="ml-2" style="font-size: 30px;">{{ orders_count }}</span></p>
+
+                                </div>
+                                <!-- Card content -->
+
+                            </div>
+                            <!-- Card -->
+
+                            <!-- Card -->
+                            <div class="card mb-4">
+
+                                <!-- Card content -->
+                                <div class="card-body">
+                                    <!-- Title -->
+                                    <h6 class="card-title">Not completed orders: </h6>
+                                    <!-- Text -->
+                                    <p class="card-text red-text"><i class="fas fa-bag-shopping fa-2x"></i><span
+                                            class="ml-2" style="font-size: 30px;">{{ not_completed_orders }}</span></p>
+
+                                </div>
+                                <!-- Card content -->
+
+                            </div>
+                            <!-- Card -->
+
+                        </div>
+                        <!-- Card group -->
+                    </div>
+                </div>
+            </div>
+        </div>
     </AdminLayout>
 </template>
 
